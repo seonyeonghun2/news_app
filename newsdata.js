72f891e6ae195f14b0d55e4382dbe40762ed3b21
seonyeonghun <seonyeonghun@kakao.com>
2025-02-18 10:53:54 +0900

    깃헙 배포

--- newsdata.js
+++ newsdata.js
@@ -1,9 +1,22 @@
 const api_key = "pub_694833507b8624b096098239a2f5b2539299c";
-const query = "pizza";
 const url = "https://newsdata.io/api/1/latest";
-const request = `${url}?apikey=${api_key}&q=${query}&language=kr`;
 
-// https://newsdata.io/api/1/archive?apikey=pub_694833507b8624b096098239a2f5b2539299c&q=example&language=en&from_date=2023-01-19&to_date=2023-01-25
+const newsForm = document.querySelector("#news_form");
 
-// 검색폼에서 쿼리(q) 입력하면, 이 문자열을 전달해서 newdata.io 에서 GET 요청하고,
-// 응답받은 결과값을 UI에 결합해서 화면에 렌더링 ==> 뉴스 앱 / 뉴스 웹 + 부트스트랩(반응형)
+newsForm.addEventListener("submit", function(e){
+    e.preventDefault();       
+    let query = document.querySelector("#keyword");
+    if (query.value == "") {
+        alert("검색어를 입력해야 합니다!");
+        query.focus();
+        return; // 함수를 종료!!
+    }
+
+    fetchNews(query.value);
+});
+
+async function fetchNews(keyword){
+    const req_url = `${url}?apikey=${api_key}&q=${keyword}`;
+    const jsonData = await fetch(req_url);
+    console.log(jsonData);
+}
